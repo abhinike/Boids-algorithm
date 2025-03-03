88af1d2c2c7e850aab4cd6f2d4b31185b5b78664
abhinike <abhishekssinha2003@gmail.com>
2025-03-03 23:25:56 +0530

    feat: abstract class for highway

--- tempCodeRunnerFile.py
+++ tempCodeRunnerFile.py
@@ -1,1 +1,2 @@
-e
+
+		if event.type == pygame.QUIT:
--- uiParameters.py
+++ uiParameters.py
@@ -5,7 +5,7 @@ panel = Panel()
 panel.color = (0, 0, 0)
 resetButton = Button("Reset")
 
-highway = Highway()
+highway = Highway1()
 highway2 = Highway2(screen_width=Width)
 
 Behaviours = TextUI("BEHAVIOURS", (Width-180, 120), (255, 255, 255))
